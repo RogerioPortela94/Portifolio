cae76c88fdf99fa2f34cd7f4e5e4a41dae698f86
Rogério Portela da Luz <64877260+RogerioPortela94@users.noreply.github.com>
2024-11-21 21:03:14 -0300

    Initial commit

---
+++ php/enviarMensagem.php
@@ -0,0 +1,21 @@
+<?php
+    $para = "rogerioporteladaluz@gmail.com";
+    $assunto = $_POST['txtAssunto'];
+    $mensagem = wordwrap($_POST['txtMensagem'], 70,"\r\n");
+    $nome = $_POST['txtNome']; 
+    $email = $_POST['txtEmail'];
+
+    $headers  = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+    $headers .= "From: $nome <$email>"."\r\n";
+    $headers .= "Reply-To: $email";
+
+$enviou = mail($para,$assunto,$mensagem,$headers);
+
+if($enviou){
+    echo "ok";
+}else{
+    echo "erro";
+}
+    
+?>
