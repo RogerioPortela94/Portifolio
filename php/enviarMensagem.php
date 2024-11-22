9ec1ce413a575b3300a6515fe20d870510865cee
Rogério Portela da Luz <64877260+RogerioPortela94@users.noreply.github.com>
2024-11-21 21:55:49 -0300

    Update enviarMensagem.php

--- php/enviarMensagem.php
+++ php/enviarMensagem.php
@@ -1,14 +1,14 @@
 <?php
     //configurar inf.php dento do xampp 
-    $para = "rogerioporteladaluz@gmail.com";
+    $para = "seuemail@email.com";
     $assunto = $_POST['txtAssunto'];
     $mensagem = wordwrap($_POST['txtMensagem'], 70,"\r\n");
     $nome = $_POST['txtNome']; 
     $email = $_POST['txtEmail'];
 
-    //$headers  = "MIME-Version: 1.0" . "\r\n";
-    //$headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
-    $headers = "From: $nome <$email>"."\r\n";
+    $headers  = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+    $headers .= "From: $nome <$email>"."\r\n";
     $headers .= "Reply-To: $email";
 
 $enviou = mail($para,$assunto,$mensagem,$headers);
